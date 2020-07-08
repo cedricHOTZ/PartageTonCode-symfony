4fedc415919077c655fb00c4f4c80ce3835813f3
cedricHOTZ <hotz.cedric@gmail.com>
2020-07-08 23:45:28 +0200

    function pour afficher au pluriel ou non en fonction du nombre de pin

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,18 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/pins", name="pins")
+     
+     * @Route("/", name="home")
      */
-    public function index()
+    public function index(EntityManagerInterface $em, PinRepository $pinRepository)
     {
+     
+    //Ajouter un pin manuellement
+
+    //  $premier = new Pin;
+    //  $premier->setTitle('lo');
+    //  $premier->setDescription('loidid');
+     
+    //  $em->persist($premier);
+    //  $em->flush();
+
+      
+       $pins = $pinRepository->findAll();
         return $this->render('pins/index.html.twig', [
-            'controller_name' => 'PinsController',
+            'pins' => $pins
         ]);
     }
+
+
+    /**
+     * @Route("/pins/create", name="create")
+     */
+    public function create(EntityManagerInterface $em): Response
+    {
+     
+        return $this->render('pins/create.html.twig'
+           
+        );
+    }
+    
 }
---
+++ src/Migrations/Version20200704194638.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200704194638 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Création de la table pin';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE pin (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE Pins');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE Pins (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, description LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('DROP TABLE pin');
+    }
+}
